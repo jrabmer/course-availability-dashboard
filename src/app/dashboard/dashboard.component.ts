09bb0c6f82ea271b4a3b36a94f8cca194c5d2f12
Josef Rabmer <jrabmer1@gmail.com>
2024-10-17 23:27:39 +0200

    Updated CSV and changed dashboard slightly

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -17,26 +17,26 @@ export interface Course {
   imports: [
     MatTableModule,   // Import Material table for styling
     MatCardModule,    // Import Material card for layout
-    MatIconModule 
+    MatIconModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  displayedColumns = ['pruefungsfach', 'modul', 'title', '2024W', '2024S', '2023W', '2023S', '2022W', '2022S', '2021W', '2021S']
+  displayedColumns = ['pruefungsfach', 'modul', 'title', '2021S', '2021W', '2022S', '2022W', '2023S', '2023W', '2024S', '2024W']
   courses: Course[] = [];
   semesters = [
-    {id: 1, name: '2024W'},
-    {id: 2, name: '2024S'}, 
-    {id: 3, name: '2023W'}, 
-    {id: 4, name: '2023S'},
-    {id: 5, name: '2022W'},
-    {id: 6, name: '2022S'},   
-    {id: 7, name: '2021W'}, 
-    {id: 8, name: '2021S'}, 
+    { id: 1, name: '2021S' },
+    { id: 2, name: '2021W' },
+    { id: 3, name: '2022S' },
+    { id: 4, name: '2022W' },
+    { id: 5, name: '2023S' },
+    { id: 6, name: '2023W' },
+    { id: 7, name: '2024S' },
+    { id: 8, name: '2024W' },
   ]
 
-  constructor(private csvService: CsvService) {}
+  constructor(private csvService: CsvService) { }
 
   ngOnInit(): void {
     this.csvService.fetchCourses().subscribe({
