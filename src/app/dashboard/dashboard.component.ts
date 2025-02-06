f65cb337a8ab47ba3be441fffde72d0e85aa30e5
Josef Rabmer <josef.rabmer@student.tuwien.ac.at>
2025-02-06 14:07:01 +0100

    Switch table layout to accordion for better organisation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { CsvService } from '../csv-reader.service';
+import {CdkAccordion, CdkAccordionItem} from '@angular/cdk/accordion';
 
 export interface Course {
   pruefungsfach: string;
@@ -17,14 +18,20 @@ export interface Course {
   imports: [
     MatTableModule,   // Import Material table for styling
     MatCardModule,    // Import Material card for layout
-    MatIconModule
+    MatIconModule,
+    CdkAccordion,
+    CdkAccordionItem
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  displayedColumns = ['pruefungsfach', 'modul', 'title', '2021S', '2021W', '2022S', '2022W', '2023S', '2023W', '2024S', '2024W']
+  displayedColumns = ['title', '2021S', '2021W', '2022S', '2022W', '2023S', '2023W', '2024S', '2024W']
+  subjects: string[] = [];
+  subjectModules = new Map<string, string[]>()
+  moduleCourses = new Map<string, Course[]>()
   courses: Course[] = [];
+
   semesters = [
     { id: 1, name: '2021S' },
     { id: 2, name: '2021W' },
@@ -42,6 +49,40 @@ export class DashboardComponent {
     this.csvService.fetchCourses().subscribe({
       next: (data) => {
         this.courses = data;
+        console.log(data)
+
+        const subjects = new Set<string>();
+
+        this.courses.forEach(course => {
+          subjects.add(course.pruefungsfach);
+        })
+
+        this.subjects = Array.from(subjects.values());
+
+        this.subjects.forEach(subject => {
+          const modules = new Set<string>();
+          this.courses.forEach(course => {
+            if (course.pruefungsfach === subject) {
+              modules.add(course.modul);
+            }
+          })
+
+          this.subjectModules.set(subject, Array.from(modules.values()))
+        })
+
+        this.subjects.forEach(subject => {
+          this.subjectModules.get(subject)?.forEach(module => {
+            const courses = new Set<Course>();
+            this.courses.forEach(course => {
+              if (course.pruefungsfach === subject && course.modul === module) {
+                courses.add(course);
+              }
+            })
+
+            this.moduleCourses.set(module, Array.from(courses.values()));
+          })
+        })
+
       },
       error: (err) => {
         console.error('Error fetching courses:', err);
